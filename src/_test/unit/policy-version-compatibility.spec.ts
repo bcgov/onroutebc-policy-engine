610aedcc61966ea924d57810e70ec75ce138c4fa
John Fletcher <john.fletcher@aot-technologies.com>
2024-12-06 23:24:27 -0800

    Add more unit tests for config version compatibility

--- src/_test/unit/policy-version-compatibility.spec.ts
+++ src/_test/unit/policy-version-compatibility.spec.ts
@@ -4,8 +4,10 @@ import { version } from '../../version';
 import semver from 'semver';
 
 describe('Policy configuration JSON version compatibility tests', () => {
-  it('should construct Policy object without any errors', async () => {
-    expect(() => new Policy(currentConfig)).not.toThrow();
+  it('should construct Policy object without any errors with matching versions', async () => {
+    const policyConfig = JSON.parse(JSON.stringify(currentConfig));
+    policyConfig.minPEVersion = version;
+    expect(() => new Policy(policyConfig)).not.toThrow();
   });
 
   it('should throw an error with an invalid semver', async () => {
@@ -30,5 +32,23 @@ describe('Policy configuration JSON version compatibility tests', () => {
     const policyConfig = JSON.parse(JSON.stringify(currentConfig));
     policyConfig.minPEVersion = semver.inc(version, 'major');
     expect(() => new Policy(policyConfig)).toThrow();
+  });
+
+  it('should throw an error if PE version ahead of policy config min version by major', async () => {
+    const policyConfig = JSON.parse(JSON.stringify(currentConfig));
+    const peMajorVer = semver.major(version);
+    if (peMajorVer > 0) {
+      policyConfig.minPEVersion = `${peMajorVer - 1}.${semver.minor(version)}.${semver.patch(version)}`;
+      expect(() => new Policy(policyConfig)).toThrow();
+    }
+  });
+
+  it('should construct without error if PE version ahead of policy config min version by minor', async () => {
+    const policyConfig = JSON.parse(JSON.stringify(currentConfig));
+    const peMinorVer = semver.minor(version);
+    if (peMinorVer > 1) {
+      policyConfig.minPEVersion = `${semver.major(version)}.${peMinorVer - 1}.${semver.patch(version)}`;
+      expect(() => new Policy(policyConfig)).not.toThrow();
+    }
   });
 });
