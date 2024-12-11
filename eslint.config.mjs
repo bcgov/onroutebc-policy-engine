b72d63bdba94ef204ff62afa22a54c88d47b2d51
John Fletcher <john.fletcher@aot-technologies.com>
2024-12-11 11:00:14 -0800

    Remove full path to tsconfig root dir in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,7 +33,7 @@ export default [{
 
         parserOptions: {
             project: ["./tsconfig.json"],
-            tsconfigRootDir: "C:\\_projects\\bcgov\\onroutebc-policy-engine",
+            tsconfigRootDir: __dirname,
         },
     },
 
