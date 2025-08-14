ab33457c9ef1c709acd109df0ba8294b51c569f9
John Fletcher <113134542+john-fletcher-aot@users.noreply.github.com>
2025-08-14 11:09:12 -0700

    feat: Implement display codes for vehicle font in PE (#62)
    * feat: Add ability for policy engine to generate display codes
    
    * Formatting code
    
    * Increment version

---
+++ src/types/display-code-defaults.ts
@@ -0,0 +1,18 @@
+export type VehicleDisplayCodeDefaults = {
+  prefixStandard: string;
+  prefixUniversal: string;
+  paddingStandard: string;
+  paddingUniversal: string;
+  spacingUniversalDefault: string;
+  spacingUniversalSmall: string;
+  spacingUniversalLarge: string;
+  spacingUniversalSmallMax: number;
+  spacingUniversalLargeMin: number;
+  extraAxleUniversal: string;
+  endAxleUniversal: string;
+  maxAxlesStandard: number;
+  thresholdAxlesUniversal: number;
+  overAxlesCodeUniversal: string;
+  universalAxleCode: string;
+  multiDigitPrefix: string;
+};
