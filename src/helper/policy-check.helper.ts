29466d61acad861236ef2b5f92e1d5a4c844b44a
glen-aot <160973940+glen-aot@users.noreply.github.com>
2026-09-03 10:19:38 -0700

    fix: ASW 6.2 to 7.2m evaluation warning message is incorrect (#138)
    Co-authored-by: Glen Ottley <gottleyaot@Glens-MacBook-Pro.local>

--- src/helper/policy-check.helper.ts
+++ src/helper/policy-check.helper.ts
@@ -1517,7 +1517,7 @@ export function CheckWheelbaseLegalLimits(
             : PolicyCheckResultType.Warning,
           message:
             hasTrailer && !hasDisallowedTrailer
-              ? 'Wheelbase for Axle Unit X and Axle Unit Y is between 6.2m and 7.2m. Semi-Trailer wheelbase must be within dimensions table found in CTPM 5.3.7.A.'
+              ? 'Wheelbase for Axle Unit 1 and Axle Unit 2 is between 6.2m and 7.2m. Semi-Trailer wheelbase must be within dimensions table found in CTPM 5.3.7.A.'
               : 'Wheelbase for Axle Unit 1 and Axle Unit 2 is between 6.2m and 7.2m. See CTPM 5.3.7.A.',
         },
       ];
